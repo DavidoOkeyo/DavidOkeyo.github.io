92f2dd397c317eda35f1b2b1311f2ce5b8ba56cb
David Okeyo <67629176+DavidoOkeyo@users.noreply.github.com>
2021-07-28 18:15:55 +0300

    Added files

---
+++ Script/main.js
@@ -0,0 +1,9 @@
+var i = 0;
+
+function myFunction() {
+    if (!i) {
+        document.getElementsByClassName("more").style.display = "inline";
+        document.getElementsByClassName("dots").style.display = "none";
+
+    }
+}
